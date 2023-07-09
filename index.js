e6df42983a4a4f57d2e1e2cc6af6a8620b6af58a
Raja1919 <rajaaart51@gmail.com>
2023-07-09 12:27:58 +0530

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import  express from "express";
 import { studentRouter } from "./Routes/student.js";
 import dotenv from "dotenv"
+import cors from "cors"
 
 dotenv.config()
 
@@ -9,7 +10,10 @@ const port=process.env.port
 
 const app=express();
 
+
 app.use(express.json())
+
+app.use(cors())
 
 app.use('/',studentRouter)
 
