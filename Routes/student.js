e7c6061e43af6773103365853d89d101532c6f9f
Raja1919 <rajaaart51@gmail.com>
2023-12-14 22:00:07 +0530

    final commit

--- Routes/student.js
+++ Routes/student.js
@@ -1,103 +1,87 @@
 import express from "express";
-import {
-  deleteStudent,
-  editStudent,
-  getAllStudent,
-  postNewStudent,
-} from "../controller/student.js";
+import { deleteStudent, editStudent, getAllStudent, postNewStudent } from "../controller/student.js";
+
 
 const router = express.Router();
+
 
 // Get All Students List:
 router.get("/all-student", async (req, res) => {
   try {
-    const students = await getAllStudent();
+    const students = await getAllStudent()
     if (!students) {
-      return res.status(400).json({ message: "No data available" });
+      return res.status(400).json({ message: "No data available"});
     }
-
+    
     res.status(200).json(students);
   } catch (error) {
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ message: "Internal server error"});
   }
 });
 
-// Get a particular student by ID:
-router.get("/get-student/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    if (!id) {
-      return res.status(400).json({ message: "No ID provided" });
-    }
-
-    // Assuming you have a function named getStudentById in your controller
-    const student = await getStudentById(id);
-
-    if (!student) {
-      return res.status(404).json({ message: "Student not found" });
-    }
-
-    res.status(200).json(student);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
 
 // Add New student:
-router.post("/add-student", async (req, res) => {
+router.post('/add-student',async (req,res)=>{
   try {
-    const NewStudent = req.body;
-    if (!NewStudent) {
-      return res.status(400).json({ message: "No data available" });
+    const NewStudent=req.body
+    if(!NewStudent){
+      return res.status(400).json({ message: "No data available"})
     }
 
-    const result = await postNewStudent(NewStudent);
-    if (!result) {
-      return res.status(400).json({ message: "posting data error" });
+    const result=await postNewStudent(NewStudent)
+    if(!result){
+      return res.status(400).json({message:"posting data error"})
     }
-    res.status(201).json(result);
+    res.status(201).json(result)
   } catch (error) {
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ message: "Internal server error"});
+    
   }
-});
+})
+
 
 // Edit Student:
-router.put("/edit-student/:id", async (req, res) => {
+router.put('/Edit-Student/:id',async (req,res)=>{
   try {
-    const { id } = req.params;
-    const UpdatedStudent = req.body;
-    if (!id || !UpdatedStudent) {
-      return res.status(400).json({ message: "No data available" });
+    const {id}=req.params
+    const UpdatedStudent=req.body
+    if( !id || !UpdatedStudent ){
+      return res.status(400).json({ message: "No data available"})
     }
 
-    const result = await editStudent(id, UpdatedStudent);
-    if (!result) {
-      return res.status(400).json({ message: "posting data error" });
+    const result=await editStudent(id,UpdatedStudent)
+    if(!result){
+      return res.status(400).json({message:"posting data error"})
     }
-    res.status(200).json({ message: "edited succesfully" });
+    res.status(200).json({message:"edited succesfully"})
   } catch (error) {
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ message: "Internal server error"});
+    
   }
-});
+})
+
 
 // Delete Student:
-router.delete("/delete-student/:id", async (req, res) => {
+router.delete("/Delete-student/:id",async(req,res)=>{
   try {
-    const { id } = req.params;
-    if (!id) {
-      return res.status(400).json({ message: "No data available" });
+    const {id}=req.params
+    if(!id){
+      return res.status(400).json({ message: "No data available"})
     }
 
-    const result = await deleteStudent(id);
-    if (!result) {
-      return res.status(400).json({ message: "posting data error" });
+    const result=await deleteStudent(id)
+    if(!result){
+      return res.status(400).json({message:"posting data error"})
     }
-    res.status(200).json({ message: "deleted succesfully" });
+    res.status(200).json({message:"deleted succesfully"})
+
   } catch (error) {
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ message: "Internal server error"});
+    
   }
-});
+})
 
-export const studentRouter = router;
+
+
+
+export const studentRouter=router
--- controller/student.js
+++ controller/student.js
@@ -1,31 +1,32 @@
 import { ObjectId } from "mongodb";
 import { client } from "../db.js";
 
-export function getAllStudent() {
-  return client.db("student-app").collection("students").find().toArray();
-}
 
-export async function getStudentById(id) {
-  return client
+export function getAllStudent(){
+    return client
     .db("student-app")
-    .collection("students")
-    .findOne({ _id: new ObjectId(id) });
+    .collection('students')
+    .find()
+    .toArray()
 }
 
-export function postNewStudent(data) {
-  return client.db("student-app").collection("students").insertOne(data);
-}
-
-export function editStudent(id, data) {
-  return client
+export function postNewStudent(data){
+    return client
     .db("student-app")
-    .collection("students")
-    .findOneAndUpdate({ _id: new ObjectId(id) }, { $set: data });
+    .collection('students')
+    .insertOne(data)
 }
 
-export function deleteStudent(id, data) {
-  return client
+export function editStudent(id,data){
+    return client
     .db("student-app")
-    .collection("students")
-    .findOneAndDelete({ _id: new ObjectId(id) });
+    .collection('students')
+    .findOneAndUpdate({_id:new ObjectId(id)},{$set:data})
+}
+
+export function deleteStudent(id,data){
+    return client
+    .db("student-app")
+    .collection('students')
+    .findOneAndDelete({_id:new ObjectId(id)})
 }
