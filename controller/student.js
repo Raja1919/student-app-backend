e6a5db57d54b60a61a1a2588a29143153efc217b
Raja1919 <rajaaart51@gmail.com>
2023-12-14 21:29:24 +0530

    first commit

--- controller/student.js
+++ controller/student.js
@@ -1,32 +1,31 @@
 import { ObjectId } from "mongodb";
 import { client } from "../db.js";
 
-
-export function getAllStudent(){
-    return client
-    .db("student-app")
-    .collection('students')
-    .find()
-    .toArray()
+export function getAllStudent() {
+  return client.db("student-app").collection("students").find().toArray();
 }
 
-export function postNewStudent(data){
-    return client
+export async function getStudentById(id) {
+  return client
     .db("student-app")
-    .collection('students')
-    .insertOne(data)
+    .collection("students")
+    .findOne({ _id: new ObjectId(id) });
 }
 
-export function editStudent(id,data){
-    return client
-    .db("student-app")
-    .collection('students')
-    .findOneAndUpdate({_id:new ObjectId(id)},{$set:data})
+export function postNewStudent(data) {
+  return client.db("student-app").collection("students").insertOne(data);
 }
 
-export function deleteStudent(id,data){
-    return client
+export function editStudent(id, data) {
+  return client
     .db("student-app")
-    .collection('students')
-    .findOneAndDelete({_id:new ObjectId(id)})
+    .collection("students")
+    .findOneAndUpdate({ _id: new ObjectId(id) }, { $set: data });
+}
+
+export function deleteStudent(id, data) {
+  return client
+    .db("student-app")
+    .collection("students")
+    .findOneAndDelete({ _id: new ObjectId(id) });
 }
